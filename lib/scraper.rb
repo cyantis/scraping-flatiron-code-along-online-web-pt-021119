0911cb2950a050b6c503c03b791fbb73978958fe
Cody Yantis <cyantis@gmail.com>
2019-03-26 02:14:48 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,14 +1,22 @@
 require 'nokogiri'
 require 'open-uri'
-require 'pry'
 
 require_relative './course.rb'
 
 class Scraper
-
-  def get_page
-      # more code coming soon!
+  
+    def print_courses
+      self.make_courses
+      Course.all.each do |course|
+        if course.title
+          puts "Title: #{course.title}"
+          puts "  Schedule: #{course.schedule}"
+          puts "  Description: #{course.description}"
+        end
+    end
   end
+  
+end
 
-end
-end
+
+
