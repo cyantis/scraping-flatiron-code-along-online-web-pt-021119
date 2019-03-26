0021be8d23d1ebc66dc7b9877320933dfe3b4349
Cody Yantis <cyantis@gmail.com>
2019-03-26 02:37:08 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -11,11 +11,11 @@ class Scraper
   end
 
   def get_courses
-    self.get_page.css(".post")
+    get_page.css(".post")
   end
 
   def make_courses
-    self.get_courses.each do |post|
+    get_courses.each do |post|
       course = Course.new
       course.title = post.css("h2").text
       course.schedule = post.css(".date").text
